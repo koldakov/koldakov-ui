cad0766958837bee712cd5e22a17bfd45d8f3c61
Ivan <jonnykold@mail.ru>
2023-07-02 23:12:03 +0200

    Add 404 page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,25 @@
+import { Col, Row, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
+import "./NotFound.css";
+
+export const NotFound = () => {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <div className="d-flex align-items-center justify-content-center pt-4">
+      <Row className="text-center">
+        <Col md={12}>
+          <p className="fs-3"> <span className="text-danger">
+            {t("Oops.text")}</span> {t("PageNotFound.text")}
+          </p>
+          <p className="lead">
+            {t("PageNotFoundDescription.text")}
+          </p>
+          <Link to="/" className="btn btn-primary">{t("GoHome.text")}</Link>
+        </Col>
+      </Row>
+    </div>
+  );
+}
