0b0dc7e0b246f6e93d645c5a31bda73f0074940b
Ivan <jonnykold@mail.ru>
2023-07-03 20:14:11 +0200

    Add cookies popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Container } from 'react-bootstrap';
 import { Route, Routes } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
+import CookieConsent from "react-cookie-consent";
 
 import { Home, NotFound } from './pages';
 import { NavbarHeader } from './components';
 import './App.css';
 
 function App() {
+  const { t } = useTranslation();
+
   return (
     <div className="app">
       <NavbarHeader />
@@ -15,6 +20,16 @@ function App() {
           <Route path="*" element={ <NotFound /> }/>
         </Routes>
       </Container>
+      <CookieConsent
+        location="bottom"
+        buttonText={t("CookieButton.text")}
+        cookieName="koldakov.com-cookies"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={150}
+        >
+          {t("CookieContent.text")}
+      </CookieConsent>
     </div>
   );
 }
